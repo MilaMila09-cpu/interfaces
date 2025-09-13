bdd21db90a37e57fe6bfeda3e344fee95cbc9731
Milena Alana <milena.2021107887@gmail.com>
2025-09-12 21:23:54 -0300

    Add files via upload

---
+++ atividadeSoma.js
@@ -0,0 +1,6 @@
+function calcularSoma(a, b) {
+    return a + b;
+}
+// Testando a função
+var resultado = calcularSoma(10, 20);
+console.log("Resultado da soma:", resultado);
---
+++ atividadeSoma.ts
@@ -0,0 +1,7 @@
+function calcularSoma(a: number, b: number): number {
+  return a + b;
+}
+
+// Testando a função
+const resultado = calcularSoma(10, 20);
+console.log("Resultado da soma:", resultado);
